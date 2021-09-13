8cac684046408f6c612fca0c14d139c64e92e11b
Andres Canavesi <andres.canavesi@gmail.com>
2021-09-13 00:28:44 -0300

    recipe form

--- src/modules/Recipe/index.js
+++ src/modules/Recipe/index.js
@@ -1,10 +1,72 @@
 import React from 'react';
 import {useParams} from "react-router";
+import {Form, Image} from "react-bootstrap";
 
 const Recipe = () => {
     const { id } = useParams();
     return ( <div>
         <h1>Recipe id: {id}</h1>
+        <Form>
+            <Form.Group className="mb-3" controlId="formTitle">
+                <Form.Label>Title</Form.Label>
+                <Form.Control type="title" placeholder="Enter recipe's title" />
+                <Form.Text className="text-muted">
+                    An awesome title for your recipe
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formDescription">
+                <Form.Label>Description</Form.Label>
+                <Form.Control as={"textarea"} type="description"
+                              style={{ height: '100px' }}
+                              placeholder="Enter recipe's description" />
+                <Form.Text className="text-muted">
+                    Describe your recipe
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formIngredients">
+                <Form.Label>Ingredients</Form.Label>
+                <Form.Control as={"textarea"}
+                              style={{ height: '100px' }}
+                              placeholder="Enter recipe's ingredients" />
+                <Form.Text className="text-muted">
+                    Ingredients for your recipe
+                </Form.Text>
+            </Form.Group>
+
+
+            <Form.Group className="mb-3" controlId="formDirections">
+                <Form.Label>Directions</Form.Label>
+                <Form.Control as={"textarea"}
+                              style={{ height: '100px' }}
+                              placeholder="Enter recipe's directions" />
+                <Form.Text className="text-muted">
+                    Directions for your recipe
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formTags">
+                <Form.Label>Tags</Form.Label>
+                <Form.Control type="tags" placeholder="Enter recipe's tags" />
+                <Form.Text className="text-muted">
+                    Comma separated tags. Example: american, sweet, cookies
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" id="formActive">
+                <Form.Text className="lead">
+                    Images
+                </Form.Text>
+                <Image src="https://res.cloudinary.com/dniiru5xy/image/upload/c_thumb,f_auto,q_60,w_300,h_200,q_auto:low/v1564258209/recipes21/bmc3laji2jowpla3fd4q.jpg" thumbnail />
+                <Image src="https://res.cloudinary.com/dniiru5xy/image/upload/c_thumb,f_auto,q_60,w_300,h_200,q_auto:low/v1564258209/recipes21/a4ctm4kkgqv1asqt6otr.jpg" thumbnail />
+            </Form.Group>
+
+            <Form.Group className="mb-3" id="formActive">
+                <Form.Check type="checkbox" label="Active" />
+            </Form.Group>
+
+        </Form>
     </div>)
 };
 
--- src/modules/Recipes/index.js
+++ src/modules/Recipes/index.js
@@ -1,41 +1,68 @@
-import React from 'react';
-import {Table} from "react-bootstrap";
+import React, {useEffect, useState} from 'react';
+import {Nav, Table} from "react-bootstrap";
+import axios from "axios";
 
-const Recipes = () => (
-    <div>
-        <h1>Recipes</h1>
-        <Table striped bordered hover responsive>
-            <thead>
-            <tr>
-                <th>#</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Username</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td>1</td>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-            </tr>
-            <tr>
-                <td>2</td>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-            </tr>
-            <tr>
-                <td>3</td>
-                <td colSpan="2">Larry the Bird</td>
-                <td>@twitter</td>
-            </tr>
-            </tbody>
-        </Table>
-    </div>
+const Recipes = () => {
 
-);
+    // here you set a state to tell the component it need to wait
+    //  until the result is fetched from the api
+    const [loadingData, setLoadingData] = useState(true);
+
+    const [data, setData] = useState([]);
+
+    const recipes = [
+        {id: 1, title: "recipe 1"},
+        {id: 2, title: "recipe 2"},
+    ];
+
+    useEffect(() => {
+        async function getData() {
+            // await axios
+            //     .get("https://jsonplaceholder.typicode.com/users")
+            //     .then((response) => {
+            //         // check if the data is populated
+            //         console.log('data loaded from remote')
+            //         console.log(response.data);
+            //         setData(response.data);
+            //         // you tell it that you had the result
+            //         setLoadingData(false);
+            //     });
+
+            setData(recipes);
+            setLoadingData(false);
+        }
+        if (loadingData) {
+            // if the result is not ready so you make the axios call
+            getData().then(() => console.log('data loaded'));
+        }
+    }, []);
+
+    return (
+        <div>
+            <h1>Recipes</h1>
+            <Table striped bordered hover responsive>
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Title</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                {data.map(item => (
+                    <tr key={item.id}>
+                        <td>{item.id}</td>
+                        <td>{item.title}</td>
+                        <td><Nav.Link href={`/recipe/edit/${item.id}`} >Edit</Nav.Link></td>
+                    </tr>
+
+                ))}
+                </tbody>
+            </Table>
+        </div>
+
+    );
+}
 
 export default {
     routeProps: {
